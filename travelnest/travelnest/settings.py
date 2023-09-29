e2a28d1a722072eb65b3d19572d435bcd14e9304
ruby222001 <rubymaharzn@gmail.com>
2023-09-29 16:34:00 +0545

    changes

--- travelnest/travelnest/settings.py
+++ travelnest/travelnest/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+# username=admin
+# password travel
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
