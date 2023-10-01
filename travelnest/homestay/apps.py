5ef2ef4431d20569e761607c49dd6db5575a3926
ruby222001 <rubymaharzn@gmail.com>
2023-10-01 17:33:21 +0545

    changes

---
+++ travelnest/homestay/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HomestayConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'homestay'
--- travelnest/travelnest/settings.py
+++ travelnest/travelnest/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 # username=admin
 # password travel
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "homestay"
+  
 ]
 
 MIDDLEWARE = [
@@ -68,6 +71,9 @@ TEMPLATES = [
             ],
         },
     },
+]
+TEMPLATE_DIRS = [
+    os.path.join(BASE_DIR, 'templates'),
 ]
 
 WSGI_APPLICATION = 'travelnest.wsgi.application'
@@ -119,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
