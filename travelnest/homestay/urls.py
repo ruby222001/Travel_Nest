4601de2042693426ac9e29c9add6b5dc6f868b63
ruby222001 <rubymaharzn@gmail.com>
2023-10-04 13:15:59 +0545

    changes

--- travelnest/homestay/urls.py
+++ travelnest/homestay/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.home,name="home"),
     path("booking/",views.booking,),
  path('user_details/', views.user_details, name='user_details'),
+ path('payment/',views.payment,name='payment'),
 ]
--- travelnest/homestay/views.py
+++ travelnest/homestay/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,"home.html")
@@ -11,5 +12,8 @@ def user_details(request):
     if request.method == 'POST':
         # Process the form data and save it to the database
         # Redirect to a success page or do what's necessary
-        return redirect('success_page')
+        return redirect('payment')
     return render(request, 'user_details_form.html')
+def payment(request):
+   
+    return render(request,'payment.html')
