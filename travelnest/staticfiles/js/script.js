228a920a8f7a56b5a9c0dc304bc0703b3523ddd8
ruby222001 <rubymaharzn@gmail.com>
2023-10-30 08:50:24 +0545

    chatbot

---
+++ travelnest/staticfiles/js/script.js
@@ -0,0 +1,9 @@
+{% load static %}
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"29af3070c16309528c276dc04e05a3345","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
