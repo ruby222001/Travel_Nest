db30aba1d83165c487cdaab5a222133f880f4f67
Rumi Maharjan <rumimaharjan99@gmail.com>
2023-10-31 11:52:16 +0545

    listing

--- travelnest/listing/apps.py
+++ travelnest/listing/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HomestayConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'homestay'
+    name = 'listing'
--- travelnest/travelnest/urls.py
+++ travelnest/travelnest/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('homestay/',include('homestay.urls')),
+    path('listing/',include('listing.urls')),
     path('admin/', admin.site.urls),
 ]
